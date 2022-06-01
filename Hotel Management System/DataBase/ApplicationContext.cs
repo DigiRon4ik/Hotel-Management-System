ef7f5edeff4cc61ca4e537b2aaf0e22267937c81
DigiRon4ik <dr.digiron@gmail.com>
2022-06-02 00:52:17 +0300

    Добавьте файлы проекта.

---
+++ Hotel Management System/DataBase/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using ServiceStack.OrmLite;
+using Hotel_Management_System.DataBase.Models;
+
+namespace Hotel_Management_System.DataBase
+{
+    internal class ApplicationContext
+    {
+        public static string connectionString = "Data\\HMS.db";
+        
+        public static IDbConnection GetDbConnection()
+        {
+            var dbFactory = new OrmLiteConnectionFactory(connectionString, SqliteDialect.Provider);
+            return dbFactory.Open();
+        }
+
+        public static void InitDB()
+        {
+            using (var db = GetDbConnection())
+            {
+                if (db.CreateTableIfNotExists<User>())
+                {
+                    db.Save(new User { Login = "Support", Password = "support", Role = "Support", Name = "Тех.", Surname = "Поддержка", Phone = "8-(800)-555-35-35" });
+                    db.Save(new User { Login = "Abdul", Password = "123", Role = "Администратор", Name = "Абдулла", Surname = "Анхаев", Phone = "8-(928)-999-75-75" });
+                }
+            }
+        }
+    }
+}
